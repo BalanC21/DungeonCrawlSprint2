042c593c534da9e105fc328adf67b1841b523e36
“BalanC21” <cristianbalan2021@gmail.com>
2022-07-14 13:32:34 +0300

    All sides attack Enemy

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Enemy.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Enemy.java
@@ -17,9 +17,9 @@ public abstract class Enemy extends Actor {
         for (int i = -1; i < 2; i++) {
             if (i == 0)
                 continue;
-            if (this.getCell().getNeighbor(i, 0).getGameMap().getPlayer().getCellType().equals(CellType.PLAYER))
+            if (this.getCell().getNeighbor(i, 0).equals(getCell().getGameMap().getPlayer().getCell()))
                 player = ((Player) this.getCell().getNeighbor(i, 0).getActor());
-            if (this.getCell().getNeighbor(0, i).getGameMap().getPlayer().getCellType().equals(CellType.PLAYER))
+            if (this.getCell().getNeighbor(0, i).equals(getCell().getGameMap().getPlayer().getCell()))
                 player = ((Player) this.getCell().getNeighbor(0, i).getActor());
         }
         return player;
