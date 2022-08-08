7cf10dfd883f3b9d7e002cdf607dc3f252a24dbf
“BalanC21” <cristianbalan2021@gmail.com>
2022-08-08 12:36:10 +0300

    Add .gitignore

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/ItemType.java
@@ -0,0 +1,17 @@
+package com.codecool.dungeoncrawl.logic;
+
+public enum ItemType {
+    SWORD("sword"),
+    KEY("key"),
+    HEALTH("health");
+
+    private final String tileName;
+
+    ItemType(String tileName) {
+        this.tileName = tileName;
+    }
+
+    public String getTileName() {
+        return tileName;
+    }
+}
